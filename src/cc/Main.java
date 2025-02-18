eec4a73befad1372c8f912073b1fa7ee739a75b2
hai12dz <fifahayrataolahai1762004@gmail.com>
2025-02-18 08:27:03 +0700

    a

--- src/cc/Main.java
+++ src/cc/Main.java
@@ -1,5 +1,12 @@
+package cc;
+
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello, World!");
+
+        System.out.println("Hello, World!");
+
     }
 }
--- src/cc/hello.java
+++ src/cc/hello.java
@@ -1,6 +1,9 @@
+package cc;
+
 public class hello {
 
     public static void main(String[] args) {
+        int a = 5;
 
 
     }
